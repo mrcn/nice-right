c413058dce6c7190f166991f57d97fcfcadeafc0
mrcn <marcin.uxa@gmail.com>
2026-02-11 11:32:46 -0600

    Add vercel.json output config

---
+++ dist/_next/static/chunks/app/page-094bcac0ada5e941.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4923:function(n,e,u){Promise.resolve().then(u.t.bind(u,46,23))},46:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=4923)}),_N_E=n.O()}]);
