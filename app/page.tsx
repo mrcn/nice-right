4b8ea82cf5a48400b049d57399a79e0700adad9e
mkd <mkd@mkds-MacBook-Pro.local>
2026-02-19 11:21:50 -0600

    Revert to v8.1 - working version

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-import Navigation from './new-site/sections/Navigation';
-import Hero from './new-site/sections/Hero';
-import Problem from './new-site/sections/Problem';
-import HowItWorks from './new-site/sections/HowItWorks';
-import Services from './new-site/sections/Services';
-import Results from './new-site/sections/Results';
-import Testimonials from './new-site/sections/Testimonials';
-import Pricing from './new-site/sections/Pricing';
-import About from './new-site/sections/About';
-import Contact from './new-site/sections/Contact';
-import Footer from './new-site/sections/Footer';
+import V81Page from './v8.1/page';
 
 export default function HomePage() {
-  return (
-    <main>
-      <Navigation />
-      <Hero />
-      <Problem />
-      <HowItWorks />
-      <Services />
-      <Results />
-      <Testimonials />
-      <Pricing />
-      <About />
-      <Contact />
-      <Footer />
-    </main>
-  );
+  return <V81Page />;
 }
